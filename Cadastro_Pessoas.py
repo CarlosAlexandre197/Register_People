987e6c1c5881de0ad0a335a16319eeec27eab815
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-03-30 03:49:03 -0300

    Update Cadastro_Pessoas.py
    Adição de campos no cadastro.

--- Cadastro_Pessoas.py
+++ Cadastro_Pessoas.py
@@ -5,22 +5,28 @@ idade = []
 sexo = []
 cidade = []
 estado = []
+curso = []
 
-for indice in range(5):
+for indice in range(6):
   
   nome = input('Digite o seu nome: ')
   id = int(input('Digite sua idade: ')) # idade
   genero = input('Qual é o seu sexo? ')
-  municipio = input('Qual a sua cidade? ')
-  est = input('Digite seu Estado: ') # estado
+  municipio = input('Qual é a sua cidade? ')
+  est = input('Digite seu estado: ') # estado
+  cur = input('Qual é o seu curso? ') # cur
 
   
   alunos.append(nome)
   idade.append(id)
   sexo.append(genero)
   cidade.append(municipio)
+  estado.append(est)
+  curso.append(cur)
 
 print(alunos)
 print(idade)
 print(sexo)
 print(cidade)
+print(estado)
+print(curso)
