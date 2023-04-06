fb81a114e504a5791bef1f40ce72b8d2600b73b4
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-04-06 00:58:12 -0300

    Update Cadastro_02.py
    Atualização do código.

--- Cadastro_02.py
+++ Cadastro_02.py
@@ -1,11 +1,36 @@
-usuarios = []
+#Cadastro
+users = [] #nome
+age = [] #idade
+gender = [] #sexo
+marital_status = [] #estado_civil
+city = [] #cidade
+country = [] #pais
+
 
 for indice in range(5):
-    opcao = int(input("O que você deseja fazer: 1) Cadastrar nome: 2) Sair: "))
+    opcao = int(input("O que você deseja fazer: 1) Cadastro: 2) Sair: "))
     if opcao == 1:
-            nome = input("Digite o seu nome: ")
-            usuarios.append(nome)
+            nome = input('Digite o seu nome: ')
+            idade = int(input('Digite a sua idade: '))
+            sexo = input('Masculino ou Feminino? ')
+            estado_civil = input('Qual o seu estado civil? ')
+            cidade = input('Qual a sua cidade? ')
+            pais = input('Digite o seu pais: ')
+            
+            users.append(nome)
+            age.append(idade)
+            gender.append(sexo)
+            marital_status.append(estado_civil)
+            city.append(cidade)
+            country.append(pais)
+
     elif opcao == 2:
         print("Programa finalizado!")
         break
-    print(usuarios)
+    
+    print(users)
+    print(age)
+    print(gender)
+    print(marital_status)
+    print(city)
+    print(country)
